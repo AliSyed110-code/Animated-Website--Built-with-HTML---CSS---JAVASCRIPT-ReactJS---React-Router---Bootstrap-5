83c9f9ab801c63a37d1eca5325f772da946dab6c
Syed Ali Sadaf <110720860+AliSyed110-code@users.noreply.github.com>
2023-12-07 16:06:56 +0530

    My-App

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from "../src/Images/Marketing.jpg";
+import App from "../src/Images/App.jpg";
+import software from "../src/Images/Software.jpg";
+import digital from "../src/Images/Digital.jpg";
+import marketing from "../src/Images/web.jpg";
+import android from "../src/Images/Android.jpg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+  {
+    imgsrc: App,
+    title: "App Development",
+  },
+  {
+    imgsrc: software,
+    title: "Software Development",
+  },
+  {
+    imgsrc: android,
+    title: "Android Development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+  },
+  {
+    imgsrc: marketing,
+    title: "Marketing",
+  },
+];
+
+export default Sdata;
